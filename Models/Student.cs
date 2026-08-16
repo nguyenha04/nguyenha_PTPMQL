12b7894c8afa50d42aa1f293c035facc3c4e08ef
“nguyenha04” <nguyenha09062004@gmail.com>
2026-08-16 21:57:06 +0700

    Initial ASP.NET Core MVC project

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DemoMVC.Models
 {
     public class Student
     {
+        [Key]
         public string? FullName { get; set; }
 
         public string? Address { get; set; }
